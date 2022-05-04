c503c5070c8be6fe868f73185dae248b54e0cee4
RICHARD WEAVER <you@example.com>
2022-05-04 12:45:23 +0200

    update to Algorithmic Curator

---
+++ Algorithmic Curator/csv_reader.py
@@ -0,0 +1,6 @@
+import pandas as pd
+pd.set_option('display_max_rows', 1000)
+datafile = pd.read_csv(data_data.csv)
+
+
+https://raw.githubusercontent.com/NationalGalleryOfArt/opendata/main/data/objects.csv
